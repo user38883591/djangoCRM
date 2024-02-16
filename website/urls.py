ea05566b8c0c3e161c9d33b2955f45e6ec1f0188
Gathungu kamau <125374979+user38883591@users.noreply.github.com>
2024-02-16 12:14:48 +0300

    added files

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('signup/',views.signup,name='signup'),
     # path('login/',views.login_user,name='login'),
     path('logout/',views.logout_user,name='logout'),
+    path('record/<int:pk>',views.Customer_record,name='record'),
+    path('delete_record/<int:pk>',views.delete_record,name='delete_record'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>',views.update_record,name='update_record'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-from .forms import SignUpForm  
+from .forms import SignUpForm,AddRecordForm 
+from.models import Record
 
 
 def index(request):
+    records = Record.objects.all()
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -18,7 +20,7 @@ def index(request):
             return redirect('index')
     else:
     
-        return render(request,'index.html')
+        return render(request,'index.html',{'records':records})
 
 def signup(request):
     if request.method == "POST":
@@ -46,6 +48,51 @@ def logout_user(request):
     logout(request)
     messages.success(request," You have logged out...")
     return redirect('index')
+
+def Customer_record(request,pk):
+    if request.user.is_authenticated:
+        Customer_record = Record.objects.get(id=pk)
+        return render(request, 'records.html', {'Customer_record': Customer_record})
+    else:
+        messages.success(request,'You have to login first!!')
+        return redirect('index')
+    
+
+def delete_record(request,pk):
+        if request.user.is_authenticated:
+            delete_it = Record.objects.get(id=pk)
+            delete_it.delete()
+            messages.success(request,'Record deleted successfully')
+            return redirect('index')
+        else:
+            messages.success(request,'You must be logged in to delete record!!!')
+            return redirect('index')
+        
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added...")
+				return redirect('index')
+		return render(request, 'add_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('index')
+def update_record(request, pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record Has Been Updated!")
+			return redirect('index')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('index')
+
     
 
 
